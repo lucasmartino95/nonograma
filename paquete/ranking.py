becbe6bb7f078dd1d68a49240df53238e7fee3ba
Lucas Martino <lucasmartino2606@gmail.com>
2025-11-29 19:28:53 -0300

    update ranking

--- paquete/ranking.py
+++ paquete/ranking.py
@@ -14,6 +14,7 @@ def leer_ranking() -> None:
         for j in range(len(ranking[i])):
             lista.append(ranking[i][j].strip())
 
+
     print("Los 3 mejores jugadores")
     for i in range(3):
         print(lista[i], end="\n")
